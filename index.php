7c8565e4792ca1a0d75b379f8f342af6fd0dbdb9
abdulhadi3115 <abhad9213@gmail.com>
2019-03-11 12:09:56 +0700

    rusak

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Kalkulator lingkaran</title>
+	<title>Kalkulator lingk</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
